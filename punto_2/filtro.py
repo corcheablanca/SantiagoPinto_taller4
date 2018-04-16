ac0b246a54085a39d170757ecc233cabb5a77877
corcheablanca <37352297+corcheablanca@users.noreply.github.com>
2018-04-16 15:35:12 -0500

    Add files via upload

---
+++ punto_2/filtro.py
@@ -0,0 +1,122 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import cmath
+pi = np.pi * 2.0
+import sys
+
+tipo=sys.argv[2]
+nombre_imagen=sys.argv[1]
+
+
+from PIL import Image
+def black_and_white():
+	global nombre_imagen
+	image = Image.open(nombre_imagen)
+	bw = image.convert('L')
+	bw.save("imagenbw.png")
+#comvierte la imagen en blanco y negro
+black_and_white()
+image=plt.imread("imagenbw.png")
+M=(np.shape(image)[0])
+N=(np.shape(image)[1])
+
+#codigo para la transformada, retorna la transformada en formato matricial 
+def FT(image):
+	global M, N
+	M=(np.shape(image)[0])
+	N=(np.shape(image)[1])
+	dft2d_b = Mat=np.zeros((M,N), dtype= complex)
+	pixels = image
+	for k in range(M):
+		for l in range(N):
+			suma = 0.0
+			for m in range(M):
+				for n in range(N):
+					e = cmath.exp(-1j*pi*(float(k*m)/M + float(l*n)/N))
+					suma = suma + pixels[m, n]* e
+			dft2d_b[l][k] = suma / M /N 
+	return (dft2d_b)
+
+#codigo para la transformada inversa, retorna la imagen en formato matricial
+def IFT(dft2d_b):
+	global M, N 
+	pixels = Mat=np.zeros((M,N), dtype= float)
+	for m in range(M):
+		for n in range(N):
+			suma = 0.0
+			for k in range(M):
+				for l in range(N):
+					e = cmath.exp(1j*pi*(float(k*m)/M + float(l*n)/N))
+					suma = suma + dft2d_b[l][k] * e
+			black= (suma.real)
+			pixels[m,n]=black
+	return (pixels)
+
+
+
+
+
+#codigo para pelar frecuencias
+def pasa_altos(transformada):
+	global M, N
+	for m in range(M):
+		for n in range(N):
+			if (abs(transformada[m,n])<0.02):
+				transformada[m,n]=0
+	return (transformada)
+
+
+#codigo para pelar frecuencias
+def pasa_bajos(transformada):
+	global M, N
+	for m in range(M):
+		for n in range(N):
+			if (abs(transformada[m,n])>0.02):
+				transformada[m,n]=0
+	return (transformada)
+
+#codigo de gaussiana
+def gauss():
+	global M, N
+	x, y = np.meshgrid(np.linspace(0.0,N-1,N), np.linspace(0.0,M-1,M))
+	d = np.sqrt(x*x+y*y)
+	sigma, mu = 1.0, 0.0
+	g = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )	
+	return (g)
+
+
+#detecta si es alto o bajo y lo hace real!
+if(tipo=="alto"):
+	imagen = pasa_altos(FT(image))
+	kernel= FT(gauss())
+
+else:
+	imagen = pasa_bajos(FT(image))
+	kernel= FT(gauss())
+
+def darnombre():
+	nombre=""
+	if(tipo=="alto"):
+		nombre= "altas.png"
+	else:
+		nombre= "bajas.png"
+
+	return nombre
+
+transform=IFT(imagen*kernel)
+plt.figure()
+plt.imshow(transform, cmap="gray")
+plt.show()
+plt.imsave(darnombre(),transform[:,:], cmap= plt.cm.gray)
+
+
+
+
+
+
+
+
+
+
+
